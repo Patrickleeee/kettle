69db748023013dd85a2b3625dc1058a0da5ff9e0
Patrick Leee <patrickleee@foxmail.com>
2018-04-22 16:35:41 +0800

    地址可配置

--- src/main/java/com/kettle/job/KettleTaskJob.java
+++ src/main/java/com/kettle/job/KettleTaskJob.java
@@ -3,6 +3,7 @@ package com.kettle.job;
 import com.kettle.uitl.KettleUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -19,20 +20,39 @@ public class KettleTaskJob {
 
     private static Logger LOGGER = LoggerFactory.getLogger(KettleTaskJob.class.getName());
 
+    @Value("${kettle.task1}")
+    private String task1;
+
+    @Value("${kettle.task2}")
+    private String task2;
+
     /**
      * 每天凌晨1点进行数据同步更新
      * @throws Exception
      */
-    @Scheduled(cron = "0 0 01 * * ?")
-    public void run() throws Exception {
+    @Scheduled(cron = "0 0 1 * * ?")
+    public void runTask1() throws Exception {
         LOGGER.info("*****kettle定时任务运行开始******");
-        String transFileName = "D:/数据处理-kettle/小贷/hana数据更新.kjb";
+        String transFileName = this.task1;
         KettleUtil.callNativeJob(transFileName);
+        LOGGER.info("*****kettle定时任务运行结束******");
+    }
+
+    /**
+     * 每周六凌晨1点进行商户每年月数据同步更新
+     * @throws Exception
+     */
+    @Scheduled(cron = "0 0 1 ? * SAT")
+    public void runTask2() throws Exception {
+        LOGGER.info("*****kettle定时任务运行开始******");
+        String transFileName = this.task2;
+        LOGGER.info("task2:{}", this.task2);
+        KettleUtil.callNativeTrans(transFileName);
         LOGGER.info("*****kettle定时任务运行结束******");
     }
 
 //    public static void main(String[] args) throws Exception {
 //        KettleTaskJob job = new KettleTaskJob();
-//        job.run();
+//        job.runTask2();
 //    }
 }
