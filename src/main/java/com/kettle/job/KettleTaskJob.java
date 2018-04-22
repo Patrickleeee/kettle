808948d04e725e0d5a1d45755ed363db4fe70c4f
Patrick Leee <patrickleee@foxmail.com>
2018-04-22 17:09:02 +0800

    remove unused code

--- src/main/java/com/kettle/job/KettleTaskJob.java
+++ src/main/java/com/kettle/job/KettleTaskJob.java
@@ -50,9 +50,4 @@ public class KettleTaskJob {
         KettleUtil.callNativeTrans(transFileName);
         LOGGER.info("*****kettle定时任务运行结束******");
     }
-
-//    public static void main(String[] args) throws Exception {
-//        KettleTaskJob job = new KettleTaskJob();
-//        job.runTask2();
-//    }
 }
